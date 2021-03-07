344a7d08e8959233f74797ab9042d241ec904f9c
Nikolay Dmukha <dmukha@mail.ru>
2021-03-07 10:50:43 +0300

    Create main code

--- src/main/java/ru/home/jspr/App.java
+++ src/main/java/ru/home/jspr/App.java
@@ -1,13 +1,63 @@
 package ru.home.jspr;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        final List<String> validPaths = List.of("/index.html", "/spring.svg", "/sprong.png");
+        try (final ServerSocket serverSocket = new ServerSocket(9999);) {
+            while (true) {
+                try (
+                        final Socket socket = serverSocket.accept();
+                        final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
+                ) {
+                    final String requestLine = in.readLine();
+                    final String[] parts = requestLine.split(" ");
+
+                    if (parts.length != 3) {
+                        //just close socket
+                        continue;
+                    }
+
+                    final String path = parts[1];
+                    if (!validPaths.contains(path)) {
+                        out.write((
+                                "HTTP/1.1 404 Not Found\r\n" +
+                                        "Content-Length: 0\r\n" +
+                                        "Connection: close\r\n" +
+                                        "\r\n"
+                        ).getBytes());
+                        out.flush();
+                        continue;
+                    }
+
+                    final Path filePath = Path.of(".", "public", path);
+                    final String mimeType = Files.probeContentType((filePath));
+                    final long length = Files.size(filePath);
+                    out.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    Files.copy(filePath, out);
+                    out.flush();
+                }
+            }
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
