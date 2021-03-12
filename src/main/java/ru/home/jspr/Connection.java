bbb4b2f5c890b76a4f5fc0280d93caa732b8afab
Nikolay Dmukha <dmukha@mail.ru>
2021-03-12 12:56:35 +0300

    Task2 branch

--- src/main/java/ru/home/jspr/Connection.java
+++ src/main/java/ru/home/jspr/Connection.java
@@ -35,6 +35,8 @@ public class Connection implements Runnable {
                 }
 
                 Request request = new Request(parts[0], parts[1], parts[2]);
+                //Some query params
+                String paramValue = request.parseQueryString().get("PARAM_NAME");
                 JsprServer.handlers.get(request.getPath()).get(request.getMethod()).handle(request, out);
 
             } catch (IOException exception) {
