b5e4cc212402437196a84b444e1f105054dc0f99
Nikolay Dmukha <dmukha@mail.ru>
2021-03-13 09:35:29 +0300

    Task2 branch

--- src/main/java/ru/home/jspr/Connection.java
+++ src/main/java/ru/home/jspr/Connection.java
@@ -7,8 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class Connection implements Runnable {
     private Socket socket;
